bab06433ec188e2c81a752e9e27ba382de32f1ae
heisje <heeje1996@gmail.com>
2024-02-27 18:07:25 +0900

    7일차 과제 완료 코 (주석 수정)

--- library-app/src/main/java/com/group/libraryapp/domain/FruitJPARepository.java
+++ library-app/src/main/java/com/group/libraryapp/domain/FruitJPARepository.java
@@ -8,15 +8,18 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FruitJPARepository extends JpaRepository<Fruit, Long> {
+        // JDBC -> JPA
         @Query("SELECT SUM(f.price) FROM Fruit f WHERE f.name = :name AND f.isSale = :isSale")
         Optional<Long> sumPriceByNameAndIsSale(@Param("name") String name, boolean isSale);
 
+        // 문제 2번
         Long countByName(String name);
 
+        // 문제 3번
         @Query("SELECT f FROM Fruit f WHERE f.isSale = TRUE AND f.price > :price")
         List<Fruit> findByPriceGTEAndIsSaleIsTrue(@Param("price") long price);
 
+        // 문제 3번
         @Query("SELECT f FROM Fruit f WHERE f.isSale = TRUE AND f.price < :price")
         List<Fruit> findByPriceLTEAndIsSaleIsTrue(@Param("price") long price);
-
 }
